4f31e3b1f00a84ae54c31613f24a56a940ca9128
Vicente Olmedo <vicente.olmedo@gmail.com>
2025-03-14 11:38:16 +0100

    fix quotes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const main = async () => {
   const name = await Name.from(Buffer.from(w3namePrivateKey, 'hex'))
   const revision = Name.Revision.decode(Buffer.from(w3nameRevision, 'hex'))
 
-  console.log('uploading ${source}')
+  console.log(`uploading ${source}`)
 
   const cid = await web3Storage.uploadFile((await filesFromPaths([source]))[0])
   console.log(`Uploaded as ${cid}`)
