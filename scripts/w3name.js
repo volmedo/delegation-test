58c5cab3c950774f6a481250bcf42cf399c300cb
Vicente Olmedo <vicente.olmedo@gmail.com>
2025-03-14 11:30:25 +0100

    fix style

--- scripts/w3name.js
+++ scripts/w3name.js
@@ -1,13 +1,13 @@
-import * as Name from "w3name"
+import * as Name from 'w3name'
 
 const name = await Name.create()
-const value = "/ipfs/empty"
+const value = '/ipfs/empty'
 const revision = await Name.v0(name, value)
 
 await Name.publish(revision, name.key)
 
-console.log("name", name.toString())
-console.log("w3name-private-key")
-console.log(name.key.bytes.toString("hex"))
-console.log("w3name-revision")
-console.log(Name.Revision.encode(revision).toString("hex"))
+console.log('name', name.toString())
+console.log('w3name-private-key')
+console.log(name.key.bytes.toString('hex'))
+console.log('w3name-revision')
+console.log(Name.Revision.encode(revision).toString('hex'))
