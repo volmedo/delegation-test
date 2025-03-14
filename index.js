70044d322b7bfb1e73abaf2890f4c477497c5e46
Vicente Olmedo <vicente.olmedo@gmail.com>
2025-03-14 11:39:31 +0100

    one more log

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ const main = async () => {
   const space = await web3Storage.addSpace(proof)
   await web3Storage.setCurrentSpace(space.did())
 
+  console.log('configuring name')
+
   const name = await Name.from(Buffer.from(w3namePrivateKey, 'hex'))
   const revision = Name.Revision.decode(Buffer.from(w3nameRevision, 'hex'))
 
